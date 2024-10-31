f5f5967297240b2d8eba24dc0e64d3a27e07804c
SwordGenius <swordgenius222@gmail.com>
2024-10-31 14:23:38 -0600

    date update stadistics schema an interface

--- src/stadistics/dto/create-stadistics.dto.ts
+++ src/stadistics/dto/create-stadistics.dto.ts
@@ -1,4 +1,5 @@
 export class CreateStadisticsDto {
   readonly plague: number;
   readonly quality: number;
+  readonly date: Date;
 }
--- src/stadistics/schemas/stadistics.schema.ts
+++ src/stadistics/schemas/stadistics.schema.ts
@@ -3,5 +3,6 @@ import * as mongoose from 'mongoose';
 export const StadisticsSchema = new mongoose.Schema({
   plague: Number,
   quality: Number,
+  date: Date,
 });
 
