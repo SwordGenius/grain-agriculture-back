949f7508ea2c7c8c5fb4d1ba4a8bd7861dd9b6da
SwordGenius <swordgenius222@gmail.com>
2024-10-31 14:13:25 -0600

    Resolving conflicts 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { UsersModule } from './users/users.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost/prueba')],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
